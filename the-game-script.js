880c5fcb061576120533b49bf526b00f84f58808
kenny-136 <kenny136@outlook.com.au>
2022-04-29 18:45:46 +1000

    Running basic Game

---
+++ the-game-script.js
@@ -0,0 +1,104 @@
+var parentBox = document.querySelector('.gameBox')
+var allDivElement= document.querySelectorAll('div')
+var progressTracker = document.querySelector('h2')
+var numOfTurns = 0
+
+var box1 = document.querySelector('#box1')
+var box2 = document.querySelector('#box2')
+var box3 = document.querySelector('#box3')
+var box4 = document.querySelector('#box4')
+var box5 = document.querySelector('#box5')
+var box6 = document.querySelector('#box6')
+var box7 = document.querySelector('#box7')
+var box8 = document.querySelector('#box8')
+var box9 = document.querySelector('#box9')
+
+
+function checkWinningCondition() {
+    if (box1.textContent == 'X' && box2.textContent == 'X' && box3.textContent == 'X') {
+        progressTracker.textContent = "Congrats Player 1 Wins"
+    } else if (box4.textContent == 'X' && box5.textContent == 'X' && box6.textContent == 'X') {
+        progressTracker.textContent = "Congrats Player 1 Wins"
+    } else if (box7.textContent == 'X' && box8.textContent == 'X' && box9.textContent == 'X') {
+        progressTracker.textContent = "Congrats Player 1 Wins"
+    } else if (box1.textContent == 'X' && box4.textContent == 'X' && box7.textContent == 'X') {
+        progressTracker.textContent = "Congrats Player 1 Wins"
+    } else if (box2.textContent == 'X' && box5.textContent == 'X' && box8.textContent == 'X') {
+        progressTracker.textContent = "Congrats Player 1 Wins"
+    } else if (box3.textContent == 'X' && box6.textContent == 'X' && box9.textContent == 'X') {
+        progressTracker.textContent = "Congrats Player 1 Wins"
+    } else if (box1.textContent == 'X' && box5.textContent == 'X' && box9.textContent == 'X') {
+        progressTracker.textContent = "Congrats Player 1 Wins"
+    } else if (box3.textContent == 'X' && box5.textContent == 'X' && box7.textContent == 'X') {
+        progressTracker.textContent = "Congrats Player 1 Wins"
+     
+    } else if (box1.textContent == 'O' && box2.textContent == 'O' && box3.textContent == 'O') {
+        progressTracker.textContent = "Congrats Player 2 Wins"
+    } else if (box4.textContent == 'O' && box5.textContent == 'O' && box6.textContent == 'O') {
+        progressTracker.textContent = "Congrats Player 2 Wins"
+    } else if (box7.textContent == 'O' && box8.textContent == 'O' && box9.textContent == 'O') {
+        progressTracker.textContent = "Congrats Player 2 Wins"
+    } else if (box1.textContent == 'O' && box4.textContent == 'O' && box7.textContent == 'O') {
+        progressTracker.textContent = "Congrats Player 2 Wins"
+    } else if (box2.textContent == 'O' && box5.textContent == 'O' && box8.textContent == 'O') {
+        progressTracker.textContent = "Congrats Player 2 Wins"
+    } else if (box3.textContent == 'O' && box6.textContent == 'O' && box9.textContent == 'O') {
+        progressTracker.textContent = "Congrats Player 2 Wins"
+    } else if (box1.textContent == 'O' && box5.textContent == 'O' && box9.textContent == 'O') {
+        progressTracker.textContent = "Congrats Player 2 Wins"
+    } else if (box3.textContent == 'O' && box5.textContent == 'O' && box7.textContent == 'O') {
+        progressTracker.textContent = "Congrats Player 2 Wins"
+    } 
+}
+
+parentBox.addEventListener('click', function(event) {
+    if (event.target.tagName === 'DIV') {
+        if (numOfTurns === 0 ) {
+        event.target.textContent = 'X'
+        numOfTurns++
+        progressTracker.textContent = numOfTurns
+        checkWinningCondition()
+    } else if (numOfTurns === 1) {
+        event.target.textContent = 'O'
+        numOfTurns++
+        progressTracker.textContent = numOfTurns
+        console.log('2nd player ', numOfTurns)
+        checkWinningCondition()
+    } else if (numOfTurns === 2) {
+        event.target.textContent = 'X'
+        numOfTurns++
+        progressTracker.textContent = numOfTurns
+        checkWinningCondition()
+    } else if (numOfTurns === 3) {
+        event.target.textContent = 'O'
+        numOfTurns++
+        progressTracker.textContent = numOfTurns
+        checkWinningCondition()
+    } else if (numOfTurns === 4) {
+        event.target.textContent = 'X'
+        numOfTurns++
+        progressTracker.textContent = numOfTurns
+        checkWinningCondition()
+    } else if (numOfTurns === 5) {
+        event.target.textContent = 'O'
+        numOfTurns++
+        progressTracker.textContent = numOfTurns
+        checkWinningCondition()
+    } else if (numOfTurns === 6) {
+        event.target.textContent = 'X'
+        numOfTurns++
+        progressTracker.textContent = numOfTurns
+        checkWinningCondition()
+    } else if (numOfTurns === 7) {
+        event.target.textContent = 'O'
+        numOfTurns++
+        progressTracker.textContent = numOfTurns
+        checkWinningCondition()
+    } else if (numOfTurns === 8) {
+        event.target.textContent = 'X'
+        numOfTurns++
+        progressTracker.textContent = "DRAW"
+        checkWinningCondition()
+    } 
+}
+})
