c7051fdd392781bcc06dc0e21590b8eb32b01499
kenny-136 <kenny136@outlook.com.au>
2022-05-02 03:17:34 +1000

    Name Editor + CSS Tweaks

--- the-game-script.js
+++ the-game-script.js
@@ -20,27 +20,42 @@ var p1ScoreBox = document.querySelector('#p1Score')
 var p2ScoreBox = document.querySelector('#p2Score')
 var gameEnd = false
 
+var p1name = document.querySelector('#player1name')
+var p2name = document.querySelector('#player2name')
+var player1NameInput = document.querySelector('#p1nameInput')
+var player2NameInput = document.querySelector('#p2nameInput')
+var p1NameBtn = document.querySelector('#p1nameBtn')
+var p2NameBtn = document.querySelector('#p2nameBtn')
+
+p1NameBtn.addEventListener('click', function() {
+    p1name.textContent = player1NameInput.value
+})
+p2NameBtn.addEventListener('click', function() {
+    p2name.textContent = player2NameInput.value
+})
+
+
 function checkWinningCondition() {
-    if ((box1.textContent == 'X' && box2.textContent == 'X' && box3.textContent == 'X') || 
-        (box4.textContent == 'X' && box5.textContent == 'X' && box6.textContent == 'X') || 
-        (box7.textContent == 'X' && box8.textContent == 'X' && box9.textContent == 'X') || 
-        (box1.textContent == 'X' && box4.textContent == 'X' && box7.textContent == 'X') || 
-        (box2.textContent == 'X' && box5.textContent == 'X' && box8.textContent == 'X') || 
-        (box3.textContent == 'X' && box6.textContent == 'X' && box9.textContent == 'X') || 
-        (box1.textContent == 'X' && box5.textContent == 'X' && box9.textContent == 'X') || 
+    if ((box1.textContent == 'X' && box2.textContent == 'X' && box3.textContent == 'X') ||
+        (box4.textContent == 'X' && box5.textContent == 'X' && box6.textContent == 'X') ||
+        (box7.textContent == 'X' && box8.textContent == 'X' && box9.textContent == 'X') ||
+        (box1.textContent == 'X' && box4.textContent == 'X' && box7.textContent == 'X') ||
+        (box2.textContent == 'X' && box5.textContent == 'X' && box8.textContent == 'X') ||
+        (box3.textContent == 'X' && box6.textContent == 'X' && box9.textContent == 'X') ||
+        (box1.textContent == 'X' && box5.textContent == 'X' && box9.textContent == 'X') ||
         (box3.textContent == 'X' && box5.textContent == 'X' && box7.textContent == 'X')) {
         progressTracker.textContent = "Congrats Player 1 Wins"
-        gameEnd = true      
+        gameEnd = true
         p1ScoreBox.textContent = ++p1ScoreTracker
 
-    } else if ((box1.textContent == 'O' && box2.textContent == 'O' && box3.textContent == 'O') || 
-    (box4.textContent == 'O' && box5.textContent == 'O' && box6.textContent == 'O') || 
-    (box7.textContent == 'O' && box8.textContent == 'O' && box9.textContent == 'O') || 
-    (box1.textContent == 'O' && box4.textContent == 'O' && box7.textContent == 'O') || 
-    (box2.textContent == 'O' && box5.textContent == 'O' && box8.textContent == 'O') || 
-    (box3.textContent == 'O' && box6.textContent == 'O' && box9.textContent == 'O') || 
-    (box1.textContent == 'O' && box5.textContent == 'O' && box9.textContent == 'O') || 
-    (box3.textContent == 'O' && box5.textContent == 'O' && box7.textContent == 'O')) {
+    } else if ((box1.textContent == 'O' && box2.textContent == 'O' && box3.textContent == 'O') ||
+        (box4.textContent == 'O' && box5.textContent == 'O' && box6.textContent == 'O') ||
+        (box7.textContent == 'O' && box8.textContent == 'O' && box9.textContent == 'O') ||
+        (box1.textContent == 'O' && box4.textContent == 'O' && box7.textContent == 'O') ||
+        (box2.textContent == 'O' && box5.textContent == 'O' && box8.textContent == 'O') ||
+        (box3.textContent == 'O' && box6.textContent == 'O' && box9.textContent == 'O') ||
+        (box1.textContent == 'O' && box5.textContent == 'O' && box9.textContent == 'O') ||
+        (box3.textContent == 'O' && box5.textContent == 'O' && box7.textContent == 'O')) {
         progressTracker.textContent = "Congrats Player 2 Wins"
         gameEnd = true
         p2ScoreBox.textContent = ++p2ScoreTracker
@@ -58,42 +73,42 @@ parentBox.addEventListener('click', function (event) {
             if (numOfTurns === 0) {
                 event.target.textContent = 'X'
                 numOfTurns++
-                progressTracker.textContent = numOfTurns
+                progressTracker.textContent = "Turn " + numOfTurns + ", " + p2name.textContent + " Turn"
                 checkWinningCondition()
             } else if (numOfTurns === 1) {
                 event.target.textContent = 'O'
                 numOfTurns++
-                progressTracker.textContent = numOfTurns
+                progressTracker.textContent = "Turn " + numOfTurns + ", " + p1name.textContent + " Turn"
                 checkWinningCondition()
             } else if (numOfTurns === 2) {
                 event.target.textContent = 'X'
                 numOfTurns++
-                progressTracker.textContent = numOfTurns
+                progressTracker.textContent = "Turn " + numOfTurns + ", " + p2name.textContent + " Turn"
                 checkWinningCondition()
             } else if (numOfTurns === 3) {
                 event.target.textContent = 'O'
                 numOfTurns++
-                progressTracker.textContent = numOfTurns
+                progressTracker.textContent = "Turn " + numOfTurns + ", " + p1name.textContent + " Turn"
                 checkWinningCondition()
             } else if (numOfTurns === 4) {
                 event.target.textContent = 'X'
                 numOfTurns++
-                progressTracker.textContent = numOfTurns
+                progressTracker.textContent = "Turn " + numOfTurns + ", " + p2name.textContent + " Turn"
                 checkWinningCondition()
             } else if (numOfTurns === 5) {
                 event.target.textContent = 'O'
                 numOfTurns++
-                progressTracker.textContent = numOfTurns
+                progressTracker.textContent = "Turn " + numOfTurns + ", " + p1name.textContent + " Turn"
                 checkWinningCondition()
             } else if (numOfTurns === 6) {
                 event.target.textContent = 'X'
                 numOfTurns++
-                progressTracker.textContent = numOfTurns
+                progressTracker.textContent = "Turn " + numOfTurns + ", " + p2name.textContent + " Turn"
                 checkWinningCondition()
             } else if (numOfTurns === 7) {
                 event.target.textContent = 'O'
                 numOfTurns++
-                progressTracker.textContent = numOfTurns
+                progressTracker.textContent = "Turn " + numOfTurns + ", " + p1name.textContent + " Turn"
                 checkWinningCondition()
             } else if (numOfTurns === 8) {
                 event.target.textContent = 'X'
@@ -106,9 +121,6 @@ parentBox.addEventListener('click', function (event) {
 }
 )
 
-rstBtn.addEventListener('click', function () {
-    resetBoard()
-})
 
 function resetBoard() {
     numOfTurns = 0
@@ -118,3 +130,6 @@ function resetBoard() {
         allBox[i].textContent = ''
     }
 }
+rstBtn.addEventListener('click', function () {
+    resetBoard()
+})
